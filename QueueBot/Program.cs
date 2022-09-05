718773ec2ebe3b0378bd6e5a81d8488dd438c55e
hlushchenko <hluushchenko@gmail.com>
2022-09-05 17:34:43 +0300

    Project Created

---
+++ QueueBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using QueueBot.Services;
+
+var hostBuilder = Host.CreateDefaultBuilder(args);
+hostBuilder.ConfigureServices(
+    services =>
+    {
+        services.AddHostedService<BotService>();
+        services.AddSingleton<HttpClient>();
+    }
+);
+
+var app = hostBuilder.Build();
+app.Run();
